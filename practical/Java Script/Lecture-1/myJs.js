1c39e11b69b18a09bd50ea0b98b994a5698dd2b2
jayswalajay <113416279+jayswalajay@users.noreply.github.com>
2022-11-17 16:32:20 +0530

    Add files via upload

---
+++ practical/Java Script/Lecture-1/myJs.js
@@ -0,0 +1,12 @@
+document.write("This is External Java Script" + "<br>")
+
+var a=10;
+let b="hello";
+const c="Tops";
+
+
+
+	document.write(a + "<br>");
+	document.write(b + "<br>");
+	document.write(c);
+	
