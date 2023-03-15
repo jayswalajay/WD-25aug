b0b42ad499cb6c1a2183d13378a0e57d46795520
jayswalajay <113416279+jayswalajay@users.noreply.github.com>
2023-03-15 12:21:48 +0530

    Add files via upload

---
+++ practical/Bootstrap/Domino's/index_files/vlb_engine/thumbscript1.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){var a=jQuery;a("#vlightbox1 .vlightbox1").mouseenter(function(){var b=a("div.vlb_zoom",this);if(!b.length){b=a('<div class="vlb_zoom" style="position:absolute">').hide().appendTo(this);a("img:first",b).detach()}b.fadeIn("fast")}).mouseleave(function(){a("div",this).fadeOut("fast")})});
