b8cc2fadce1b292d7de5db6a8c91dbedeff173ab
jayswalajay <113416279+jayswalajay@users.noreply.github.com>
2023-03-16 10:45:15 +0530

    Add files via upload

---
+++ practical/Bootstrap/Domino's/lightbox/index_files/index_files/vlb_engine/vlbdata1.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:true,borderSize:21,classNames:'vlightbox1',descSliding:true,enableRightClick:false,enableSlideshow:true,overlayOpacity:0.4,prefix:'vlb1',resizeSpeed:7,slideTime:4,startZoom:true}) });
